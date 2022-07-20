6404a708a4627963706ed82a7965a579cb479a4a
lohith <lohith.mp@joshsoftware.com>
2022-07-20 17:18:55 +0530

    Program7 commited

---
+++ src/Program_7/Atm.java
@@ -0,0 +1,39 @@
+package Program_7;
+import java.util.Scanner;
+
+public class Atm 
+{
+	int atm_pin=4512;
+	public void Atmpin(int entered_atm_pin) throws IncorrectPinException  //throws keyword to display the exception
+	{
+			if(atm_pin==entered_atm_pin)
+			{
+				System.out.println("Your pin is correct");
+			}
+			else
+			{
+				throw new IncorrectPinException("Please enter correct pin");   // using throw keyword to display precise sequence of steps leading to the exception
+			}
+	}
+	public static void main(String[] args) 
+	{
+		Atm a= new Atm();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your atm pin");
+		//try block to check the risky line of code
+		try 
+		{
+			int entered_atmpin=sc.nextInt();
+			a.Atmpin(entered_atmpin);
+		}
+		//catch block to handle the exception
+		catch(IncorrectPinException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		finally
+		{
+			System.out.println("Thank You");
+		}		
+	}
+}
---
+++ src/Program_7/IncorrectPinException.java
@@ -0,0 +1,11 @@
+package Program_7;
+
+public class IncorrectPinException extends RuntimeException
+{
+
+	public IncorrectPinException(String s) 
+	{
+		super(s);
+	}
+ 
+}
