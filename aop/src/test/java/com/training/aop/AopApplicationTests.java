792498e247c7de203a40aeb43f750ac398de5950
lohithmp <109272620+lohithmp@users.noreply.github.com>
2022-09-29 15:12:05 +0530

    Add files via upload

---
+++ aop/src/test/java/com/training/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
